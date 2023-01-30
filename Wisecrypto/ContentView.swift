bead4a766e7a7cf7d1ad4e7ff7e3052929944828
Yaroslav Orlov <ulverforest@gmail.com>
2023-01-30 12:53:23 +0200

    Initial Commit

---
+++ Wisecrypto/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wisecrypto
+//
+//  Created by Yaroslav Orlov on 30.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Wisecrypto/WisecryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WisecryptoApp.swift
+//  Wisecrypto
+//
+//  Created by Yaroslav Orlov on 30.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct WisecryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
