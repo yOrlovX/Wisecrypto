682c03e1e5914b81b22e7e03b9a1afb5bd8314a1
Yaroslav Orlov <ulverforest@gmail.com>
2023-02-15 11:56:14 +0200

    add PaymentView

--- Wisecrypto/Profile/PaymentView.swift
+++ Wisecrypto/Profile/PaymentView.swift
@@ -8,8 +8,26 @@
 import SwiftUI
 
 struct PaymentView: View {
+  let payments = PaymentModel.paymentData
+  
     var body: some View {
-        Text("Payment view")
+      List(payments) { payment in
+        HStack {
+          Image(payment.image)
+            .resizable()
+            .scaledToFit()
+            .frame(width: 64, height: 64)
+          
+          VStack(alignment: .leading) {
+            Text(payment.name)
+              .font(.system(size: 20, weight: .bold))
+            
+            Text(payment.billNumber)
+              .font(.system(size: 16, weight: .regular))
+              .foregroundColor(.gray)
+          }
+        }
+      }
     }
 }
 
@@ -17,4 +35,16 @@ struct PaymentView_Previews: PreviewProvider {
     static var previews: some View {
         PaymentView()
     }
+}
+
+struct PaymentModel: Identifiable {
+  let id = UUID()
+  let image: String
+  let name: String
+  let billNumber: String
+  
+  static let paymentData = [
+    PaymentModel(image: "ovo", name: "OVO", billNumber: "081264950021"),
+    PaymentModel(image: "goPay", name: "GOPAY", billNumber: "081264956578")
+  ]
 }
