16dc3095e9d7a489765db3b8cb85fa0beaec7025
Yaroslav Orlov <ulverforest@gmail.com>
2023-04-01 13:17:51 +0300

    add some profile changes

--- Wisecrypto/Profile/PrivacyView.swift
+++ Wisecrypto/Profile/PrivacyView.swift
@@ -10,10 +10,9 @@ import SwiftUI
 struct PrivacyView: View {
     var body: some View {
       ZStack {
-        Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus tincidunt nibh porttitor tempor mollis. Morbi dolor lacus, vehicula vitae magna nec, condimentum mattis ante. Sed sagittis quam nec ornare aliquam. Proin ullamcorper dignissim placerat. Donec mauris risus, molestie vitae ornare a, pellentesque ac mi. Aliquam erat volutpat. Suspendisse potenti. Fusce ultrices lacus sapien, et facilisis quam condimentum at. Aenean tellus ligula, feugiat nec consequat quis, malesuada sit amet turpis. Vestibulum egestas odio in arcu accumsan, sed tempor erat rhoncus.")
-          .padding(.horizontal, 10)
+        Text("Coming soon 🙂")
+          .font(.headline)
       }
-        
     }
 }
 
